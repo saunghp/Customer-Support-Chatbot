3aaffc8806b4fdfd359afa7da871f93782460560
saunghp <saunghninphyu122133@gmail.com>
2026-04-07 16:55:35 +0800

    Fix project structure

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ChatApp from "./chatApp";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChatApp />
-);
